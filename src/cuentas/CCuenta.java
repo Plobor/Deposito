67d4cc60093fb08f1fa0bf3117afc8f8010373d4
Andres <roguez.andres@gmail.com>
2022-05-24 11:44:24 +0100

    Segundo commit

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -11,7 +11,7 @@ public class CCuenta {
     }
 
     public CCuenta(String nom, String cue, double sal, double tipo) {
-        nombre = nom;
+        nombre = nom; // Modificación para segundo commit
         cuenta = cue;
         saldo = sal;
     }
